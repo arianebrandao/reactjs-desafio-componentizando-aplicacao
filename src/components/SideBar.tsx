d16cd783810909ba79c6a7fe792e883ffdcc9930
Ariane Brandao <ariane@ghbranding.com.br>
2022-06-14 13:09:30 -0300

    feat: feito desafio otimizando a aplicação

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,46 +1,33 @@
-import { useEffect, useState } from "react";
-import { api } from '../services/api';
+import { Button } from "../components/Button";
 
-import { Button } from '../components/Button';
+import "../styles/sidebar.scss";
 
-import '../styles/sidebar.scss';
-
-interface GenreResponseProps {
-  id: number;
-  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
-  title: string;
-}
-
-interface SidebarProps{
+interface SideBarProps {
+  genres: Array<{
+    id: number;
+    name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+    title: string;
+  }>;
   selectedGenreId: number;
-  setSelectedGenreId: (setSelectedGenreId: number) => void; // setSelectedGenreId() é uma função
+  buttonClickCallback: (args: any) => void;
 }
 
-export function SideBar({ selectedGenreId, setSelectedGenreId }: SidebarProps) {
-  // Complete aqui
-  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
-
-  useEffect(() => {
-    api.get<GenreResponseProps[]>('genres').then(response => {
-      setGenres(response.data);
-    });
-  }, []);
-
-  function handleClickButton(id: number) {
-    setSelectedGenreId(id);
-  }
-
+export function SideBar({
+  genres,
+  selectedGenreId,
+  buttonClickCallback,
+}: SideBarProps) {
   return (
     <div className="buttons-container">
-      {genres.map(genre => (
+      {genres.map((genre) => (
         <Button
           key={String(genre.id)}
           title={genre.title}
           iconName={genre.name}
-          onClick={() => handleClickButton(genre.id)}
+          onClick={() => buttonClickCallback(genre.id)}
           selected={selectedGenreId === genre.id}
         />
       ))}
     </div>
-  )
+  );
 }
